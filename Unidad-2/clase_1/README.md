628d4a25580d88ab43bca6083f60f3b05ace7c2d
Diego Hernandez <hcdiego2012@gmail.com>
2025-10-02 20:08:42 -0600

    Clase 1: Widgets basicos

---
+++ Unidad-2/clase_1/README.md
@@ -0,0 +1,14 @@
+# clase_1
+
+Clase 1 - Empezando a ver flutter por primera vez.
+
+### Comandos utilizados en clase:
+> flutter --version
+> flutter devices
+> flutter pub get
+
+- En el menu de vscode hacer
+> flutter:select device
+
+- y seleccionar un dispositivo
+- luego en el archivo de lib/main hacer click derecho y "Run without debbuging". 
---
+++ Unidad-2/clase_1/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const paddingSize = EdgeInsets.all(10.0);
+    const textStyle = TextStyle(
+      color: Colors.lightBlueAccent,
+      fontSize: 20,
+      fontWeight: FontWeight.bold
+      );
+
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Padding(padding: paddingSize,
+              child: Text('Hello World 1', style: textStyle,),
+            ),
+            Padding(padding: paddingSize,
+              child: Text('Hello World 2', style: textStyle,),
+            ),
+            Padding(padding: paddingSize,
+              child: Text('Hello World 3', style: textStyle,),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0), 
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+                children: [
+                  Text('Hello World 4', style: TextStyle(color: Colors.red),),
+                  Text('Hello World 5'),
+                  Text('Hello World 6'),
+                ]
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.lightBlueAccent),
+                foregroundColor: MaterialStateProperty.all(Colors.white)
+
+              ),
+              child: Text("Click Me 1")
+            ),
+            TextButton(
+              onPressed: () {}, 
+              child: 
+              Text("Click Me 2")
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child:
+              Text("Click Me 3") 
+            ),
+            IconButton(
+              onPressed: () { 
+                print("Click Me 4"); 
+              },
+              icon: Icon(
+                Icons.save,
+                color: const Color.fromARGB(255, 233, 90, 90)
+              )
+            )
+          ],),
+        ),
+      ),
+    );
+  }
+}
